7377196f5cfc0f638f77973f19da4c30168dd0f8
Rishat_Imangulov <gr40in@yandex.ru>
2023-02-27 04:27:15 +0500

    тест input count

--- CodeFolder/Program.cs
+++ CodeFolder/Program.cs
@@ -1,7 +1,7 @@
 ﻿void Main()
 {
     System.Console.WriteLine("Программа разложит введённую строку на массив из слов," +
-    " \nа затем выведет те элементы массива, длинна которых меньше или равны 3.");
+    " \nа затем выведет те элементы массива, длинна которых меньше или равны числу, которое будет указано далее.");
 
     string s = EnterString("Введите строку: "); // "12 34 56 78";
 
@@ -11,14 +11,15 @@
 
     System.Console.WriteLine(String.Join(", ", arr));
 
+    int countSrt = EnterCount("Введите длинну строки для отбора: ");
+    System.Console.WriteLine(countSrt);
+
 }
 
 string[] SplitStringToArray(string s)
 {
     char[] separators = new char[] { ' ', '.', ',', '!', ';', '?' };
-
     string[] subS = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
-
     return subS;
 }
 
@@ -26,6 +27,22 @@ string EnterString(string welcomeToInput)
 {
     System.Console.Write(welcomeToInput);
     return Console.ReadLine()!;
+}
+int EnterCount(string welcomeToInput)
+{
+    bool inputOk = false;
+    int count = 0;
+    while (!inputOk)
+    {
+        System.Console.Write(welcomeToInput);
+        string inp = Console.ReadLine()!;
+        inputOk = int.TryParse(inp, out int n);
+        if (inputOk)
+            count = n;
+        else
+            System.Console.Write("Вы ввели не число. ");
+    }
+    return count;
 }
 
 Main();
