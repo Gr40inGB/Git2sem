7f0a31dc5012b4577bb9d37a47184d4d22a9bae4
Rishat_Imangulov <gr40in@yandex.ru>
2023-02-27 03:14:26 +0500

    тест ввода

--- CodeFolder/Program.cs
+++ CodeFolder/Program.cs
@@ -3,8 +3,16 @@
     System.Console.WriteLine("Программа разложит введённую строку на массив из слов," +
     " \nа затем выведет те элементы массива, длинна которых меньше или равны 3.");
 
-    string s = "12 34 56 78";//EnterString("Введите строку: ");
+    string s = EnterString("Введите строку: "); // "12 34 56 78";
 
+    System.Console.WriteLine(s);
+
+}
+
+string EnterString(string welcomeToInput)
+{
+    System.Console.Write(welcomeToInput);
+    return Console.ReadLine()!;
 }
 
 Main();
